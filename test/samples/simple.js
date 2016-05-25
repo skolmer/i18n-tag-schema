0369cc7e489aa61cd6f0fc1b359c5e5b95473140
Steffen Kolmer <steffen@kolmer.net>
2016-05-25 19:55:26 +0200

    Added multiline support

--- test/samples/simple.js
+++ test/samples/simple.js
@@ -1,1 +1,9 @@
-i18n`Hello ${ name }, you have ${ amount }:c in your bank account.`
+console.log(i18n`Hello ${ name }, you have ${ amount }:c in your bank account.`)
+
+console.log(i18n`
+    <users>
+    ${hello.map((item) => i18n`
+        <user name="${item.name}">${item.percentage}:p</user>
+    `).join('')}
+    </users>
+`)
--- test/test-i18n-tag-schema.js
+++ test/test-i18n-tag-schema.js
@@ -2,43 +2,41 @@
 import path from 'path';
 import fs from 'fs';
 import assert from 'assert';
-import i18nTagSchema from '../dist/lib';
+import i18nTagSchema from '../lib';
+
+ const expected = {
+    "type": "object",
+    "properties": {
+        "Hello ${0}, you have ${1} in your bank account.": {
+            "type": "string"
+        },
+        "\r\n        <user name=\"${0}\">${1}</user>\r\n    ": {
+            "type": "string"
+        },
+        "\r\n    <users>\r\n    ${0}\r\n    </users>\r\n": {
+            "type": "string"
+        }
+    },
+    "additionalProperties": false
+}
 
 describe('i18n-tag-schema', () => {
     it(`should match json string`, (done) => {
-        const actual = {
-            "type": "object",
-            "properties": {
-                "Hello ${0}, you have ${1} in your bank account.": {
-                    "type": "string"
-                }
-            },
-            "additionalProperties": false
-        }
         const filter = '\\.jsx?'
         const srcPath = path.resolve(__dirname, './samples')
         i18nTagSchema(srcPath, filter, null, (log) => {
-            assert.equal(JSON.stringify(actual), JSON.stringify(log));
+            assert.equal(JSON.stringify(log), JSON.stringify(expected));
             done();
         })
     })
 
     it(`should match json file`, (done) => {
-        const actual = {
-            "type": "object",
-            "properties": {
-                "Hello ${0}, you have ${1} in your bank account.": {
-                    "type": "string"
-                }
-            },
-            "additionalProperties": false
-        }
         const filter = '\\.jsx?'
         const srcPath = path.resolve(__dirname, './samples')
         const schema = path.resolve(__dirname, './samples/schema.json')
         i18nTagSchema(srcPath, filter, schema, () => {
             let prevJson = fs.readFileSync(schema, 'utf-8')
-            assert.equal(JSON.stringify(actual), JSON.stringify(JSON.parse(prevJson)));
+            assert.equal(JSON.stringify(JSON.parse(prevJson)), JSON.stringify(expected));
             done();
         })
     })
