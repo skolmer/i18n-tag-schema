52a57a22cc3bdbd98722e49bfc0cc8f6b68eb99a
steffenk <steffen@kolmer.net>
2016-07-03 13:32:40 +0200

    Added command line interface. This closes #2

---
+++ cli/index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+var program = require('commander');
+var i18nTagSchema = require('../dist/lib').default;
+var colors = require('colors');
+
+program
+    .version('0.0.1')
+    .usage('<path> [options]')
+    .option('-s, --schema <path>', 'set schema path. defaults to ./translation.schema.json')
+    .option('-f, --filter <regex>', 'a regular expression to filter source files. defaults to \\\\.jsx?')
+    .option('-g, --groups', 'group translations by module filenames')
+    .action(function (path) {
+        i18nTagSchema(path, program.filter || '\\.jsx?', program.schema || './translation.schema.json', program.groups, (output, type) => {
+            switch (type) {
+                case 'info':
+                    console.log(output);
+                    break;
+                case 'warning':
+                    console.log(colors.yellow(output));
+                    break;
+                case 'error':
+                    console.log(colors.red(output));
+                    break;
+                case 'success':
+                    console.log(colors.green(output));
+                    break;
+            }
+        })
+    })
+    .parse(process.argv);
+
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,7 @@
 /// <reference path="globals/babel-traverse/index.d.ts" />
 /// <reference path="globals/babylon/index.d.ts" />
+/// <reference path="globals/colors/index.d.ts" />
+/// <reference path="globals/commander/index.d.ts" />
 /// <reference path="globals/escape-string-regexp/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/rimraf/index.d.ts" />
