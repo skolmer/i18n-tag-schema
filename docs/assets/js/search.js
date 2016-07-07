2495e6f6466e69138a9d06aacd3b483bc95b8c4a
steffenk <steffen@kolmer.net>
2016-07-08 01:01:57 +0200

    Initial docs subtree commit

---
+++ docs/assets/js/search.js
@@ -0,0 +1,1 @@
+﻿var typedoc = typedoc || {};typedoc.search = typedoc.search || {};typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"func","url":"globals.html#func","classes":"tsd-kind-function"},{"id":1,"kind":64,"name":"templatesFromFile","url":"globals.html#templatesfromfile","classes":"tsd-kind-function"}]};
