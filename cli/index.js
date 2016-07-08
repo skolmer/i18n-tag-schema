ec601b69bcbf78b96ff1fd369c3ff40dc1e03cab
steffenk <steffen@kolmer.net>
2016-07-08 22:55:06 +0200

    Optimized console output

--- cli/index.js
+++ cli/index.js
@@ -8,6 +8,34 @@ var colors = require('colors');
 var pathLib = require('path');
 var fs = require('fs');
 
+function formatResult(message) {
+    var percentage = message.match(/(\d+)\s*%\s*translated/);
+    if(percentage) {
+        var val = Number.parseInt(percentage[1]);
+        if(val < 50) {
+            message = message.replace(percentage[0], colors.bgRed(colors.white(percentage[0])));            
+        } else if(val < 100) {
+            message = message.replace(percentage[0], colors.bgYellow(colors.black(percentage[0])));  
+        }  else {
+            message = message.replace(percentage[0], colors.bgGreen(colors.white(percentage[0]))); 
+        }     
+    }
+    var missing = message.match(/(\d+)\s*missing/);
+    if(missing) {
+        var val = Number.parseInt(missing[1]);
+        if(val === 0) {
+            message = message.replace(missing[0], colors.bgGreen(colors.white(missing[0])));
+        } else {
+            message = message.replace(missing[0], colors.bgRed(colors.white(missing[0])));
+        }
+    }
+    var invalid = message.match(/(\d+)\s*invalid/);
+    if(invalid) {
+        message = message.replace(invalid[0], colors.bgYellow(colors.black(percentage[0])));
+    }
+    return message;
+}
+
 program
     .version('0.0.1')
     .usage('<path> [options]')
@@ -20,37 +48,37 @@ program
     .action(function (path) {
         if(program.validate) {
             if(!program.schema) {
-                console.log('  error: option `--schema <path>` missing');
+                console.log('  ' + colors.bgRed(colors.white('error:')) + ' ' +'option `--schema <path>` missing');
                 process.exit(1);
             }
             if(pathLib.extname(path) !== '.json') {
-                console.log('  error: ' + path + ' is not a json file.');
+                console.log('  ' + colors.bgRed(colors.white('error:')) + ' ' + path + ' is not a json file.');
                 process.exit(1);
             }
             vaidateSchema(path, program.schema, (output, type) => {
                 switch (type) {
                     case 'info':
-                        console.log('  info: ' + output);
+                        console.log('  ' + colors.bgWhite(colors.black('info:')) + ' ' + output);
                         break;
                     case 'warn':
-                        console.log(colors.yellow('  warn: ' + output));
+                        console.log('  ' + colors.bgYellow(colors.black('warn:')) + ' ' + output);
                         break;
-                    case 'error':
-                        console.log(colors.red('  error: ' + output));
+                    case 'error':                        
+                        console.log('  ' + colors.bgRed('X invalid:') + ' ' + formatResult(output));
                         process.exit(1);
                         break;
                     case 'success':
-                        console.log(colors.green('  success: ' + output));
+                        console.log('  ' + colors.bgGreen(colors.white('√ valid:')) + ' ' + formatResult(output));
                         break;
                 }
             });
         } else if(program.export || program.filter) {
             if(program.export && !pathLib.extname(program.export).match(program.filter || '\\.jsx?')) {
-                console.log('  error: ' + program.export + ' is not a JavaScript file.');
+                console.log('  ' + colors.bgRed(colors.white('error:')) + ' ' +program.export + ' is not a JavaScript file.');
                 process.exit(1);
             }
             if(program.target && pathLib.extname(program.target) !== '.json') {
-                console.log('  error: ' + program.target + ' is not a json file.');
+                console.log('  ' + colors.bgRed(colors.white('error:')) + ' ' +program.target + ' is not a json file.');
                 process.exit(1);
             }
             templatesFromFile(path, program.export, program.groups,
@@ -58,17 +86,17 @@ program
                 if(program.target) {
                     switch (type) {
                         case 'info':
-                            console.log('  info: ' + output);
+                            console.log('  ' + colors.bgWhite(colors.black('info:')) + ' ' + output);
                             break;
                         case 'warn':
-                            console.log(colors.yellow('  warn: ' + output));
+                            console.log('  ' + colors.bgYellow(colors.black('warn:')) + ' ' + output);
                             break;
                         case 'error':
-                            console.log(colors.red('  error: ' + output));
+                            console.log('  ' + colors.bgRed(colors.white('error:')) + ' ' + output);
                             process.exit(1);
                             break;
                         case 'success':
-                            console.log(colors.green('  success: ' + output));
+                            console.log('  ' + colors.bgGreen(colors.white('success:')) + ' ' + output);
                             break;
                     }
                 }
@@ -77,11 +105,11 @@ program
                 if(program.target) {
                     fs.writeFile(program.target, JSON.stringify(JSON.parse(templates), null, 2), 'utf-8', function (err) {
                         if (err) {
-                            console.log(colors.red('  error: ' + err.message));
+                            console.log('  ' + colors.bgRed(colors.white('error:')) + ' ' + err.message);
                             process.exit(1);
                             return;
                         }
-                        console.log(colors.green('  success: Exported translation keys to ' + program.export));
+                        console.log('  ' + colors.bgGreen(colors.white('success:')) + ' Exported translation keys to ' + program.export);
                     });
                 } else {
                     console.log(JSON.stringify(JSON.parse(templates), null, 2));
@@ -91,17 +119,17 @@ program
             i18nTagSchema(path, program.filter || '\\.jsx?', program.schema || './translation.schema.json', program.groups, (output, type) => {
                 switch (type) {
                     case 'info':
-                        console.log(output);
+                        console.log('  ' + colors.bgWhite(colors.black('info:')) + ' ' + output);
                         break;
                     case 'warn':
-                        console.log(colors.yellow(output));
+                        console.log('  ' + colors.bgYellow(colors.black('warn:')) + ' ' + output);
                         break;
                     case 'error':
-                        console.log(colors.red(output));
+                        console.log('  ' + colors.bgRed(colors.white('error:')) + ' ' + output);
                         process.exit(1);
                         break;
                     case 'success':
-                        console.log(colors.green(output));
+                        console.log('  ' + colors.bgGreen(colors.white('success:')) + ' ' + output);
                         break;
                 }
             })
