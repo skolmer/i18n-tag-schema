a21658c8ca6a956bff7eeeebed91142f7f43be9f
Steffen Kolmer <steffen@kolmer.net>
2016-11-01 18:00:05 +0100

    refactor(i18nTagSchema): restructured code and improved test coverage

--- __tests__/exportTranslationKeys.test.js
+++ __tests__/exportTranslationKeys.test.js
@@ -2,10 +2,64 @@ import path from 'path'
 import { exportTranslationKeys } from '../lib'
 
 describe('exportTranslationKeys', () => {
-     it('should export templates as array', (done) => {
+    it('should fail if rootPath param is missing', (done) => {
+      const filePath = './samples/grouped.js'
+      exportTranslationKeys({
+          filePath, 
+          logger: { toConsole: true },
+          callback: (status, templates) => {
+                expect(status).toEqual(1)
+                expect(templates).toEqual('rootPath is not defined.')
+                done()                
+            }
+        })
+    })
+
+    it('should export all templates from a directory', (done) => {
+      const rootPath = path.resolve(__dirname, './samples')
+      exportTranslationKeys({
+          rootPath, 
+          logger: { toConsole: true },
+          callback: (status, templates) => {
+                expect(status).toEqual(0)
+                expect(templates).toEqual([
+                    '\n        <user name="${0}">${1}</user>\n    ',
+                    '\n    <users>\n    ${0}\n    </users>\n',
+                    {
+                        'group': 'custom group',
+                        'items': [
+                            'Hello ${0}, you have ${1} in your bank account.'
+                        ]
+                    },
+                    {
+                        'group': 'custom group 2',
+                        'items': [
+                            'Hello ${0}, you have ${1} in your bank account.'
+                        ]
+                    },
+                    {
+                        'group': 'custom inline group',
+                        'items': [
+                            'Hello!',
+                            'Welcome!'
+                        ]
+                    },
+                    {
+                        'group': 'grouped.js',
+                        'items': [
+                            'Hello ${0}, you have ${1} in your bank account.',
+                            'Hello!'
+                        ]
+                    }
+                ])
+                done()                
+            }
+        })
+    })
+
+     it('should export grouped templates as array', (done) => {
         const rootPath = path.resolve(__dirname, './samples')
         const filePath = path.resolve(__dirname, './samples/grouped.js')
-        const filePath2 = path.resolve(__dirname, './samples/multiline.js')
         exportTranslationKeys({
           rootPath, 
           filePath, 
@@ -42,23 +96,30 @@ describe('exportTranslationKeys', () => {
                         ]
                     }
                 ])
-                exportTranslationKeys({
-                  rootPath, 
-                  filePath: filePath2,
-                  callback: (status, templates) => {
-                        expect(status).toEqual(0)
-                        expect(templates).toEqual([
-                            '\n        <user name="${0}">${1}</user>\n    ',
-                            '\n    <users>\n    ${0}\n    </users>\n',
-                            {
-                                'group': 'custom inline group',
-                                'items': ['Hello!']
-                            }
-                        ])
-                        done()
-                    }
-                })
+                done()                
             }
         })
     })
+
+    it('should export multiline templates as array', (done) => {
+        const rootPath = path.resolve(__dirname, './samples')
+        const filePath = path.resolve(__dirname, './samples/multiline.js')
+        exportTranslationKeys({
+          rootPath, 
+          filePath,
+          callback: (status, templates) => {
+                expect(status).toEqual(0)
+                expect(templates).toEqual([
+                    '\n        <user name="${0}">${1}</user>\n    ',
+                    '\n    <users>\n    ${0}\n    </users>\n',
+                    {
+                        'group': 'custom inline group',
+                        'items': ['Hello!']
+                    }
+                ])
+                done()
+            }
+        })
+    })
+    
 })
--- __tests__/validateTranslations.test.js
+++ __tests__/validateTranslations.test.js
@@ -2,18 +2,59 @@ import path from 'path'
 import { validateTranslations } from '../lib'
 
 describe('validateTranslations', () => {
-    it('should validate translations', (done) => {
-        const schemaPath = path.resolve(__dirname, './samples/schema.json')
-        const translationPath = path.resolve(__dirname, './samples/translation.json')
-        validateTranslations({
-          rootPath: translationPath, 
-          schemaPath, 
-          logger: { toConsole: true },
-          callback: (status, result) => {
-            expect(status).toEqual(1)
-            expect(result).toEqual('translation.json has 3 missing translations; 63% translated.')
-            done()
-          }
-        })
+  it('should fail if rootPath param is missing', (done) => {
+    const schemaPath = path.resolve(__dirname, './samples/schema.json')
+    validateTranslations({
+      schemaPath,
+      logger: { toConsole: true },
+      callback: (status, result) => {
+        expect(status).toEqual(1)
+        expect(result).toEqual('rootPath is not defined.')
+        done()
+      }
     })
+  })
+
+  it('should fail if schemaPath param is missing', (done) => {
+    const rootPath = path.resolve(__dirname, './samples')
+    validateTranslations({
+      rootPath,
+      logger: { toConsole: true },
+      callback: (status, result) => {
+        expect(status).toEqual(1)
+        expect(result).toEqual('schemaPath is not defined.')
+        done()
+      }
+    })
+  })
+
+  it('should validate translations', (done) => {
+    const schemaPath = path.resolve(__dirname, './samples/schema.json')
+    const rootPath = path.resolve(__dirname, './samples')
+    validateTranslations({
+      rootPath,
+      schemaPath,
+      logger: { toConsole: true },
+      callback: (status, result) => {
+        expect(status).toEqual(1)
+        expect(result).toEqual('translation.json has 3 missing translations and 1 invalid key; 63% translated.')
+        done()
+      }
+    })
+  })
+
+  it('should validate single translation file', (done) => {
+    const schemaPath = path.resolve(__dirname, './samples/schema.json')
+    const translationPath = path.resolve(__dirname, './samples/translation.json')
+    validateTranslations({
+      rootPath: translationPath,
+      schemaPath,
+      logger: { toConsole: true },
+      callback: (status, result) => {
+        expect(status).toEqual(1)
+        expect(result).toEqual('translation.json has 3 missing translations and 1 invalid key; 63% translated.')
+        done()
+      }
+    })
+  })
 })
