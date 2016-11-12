e47c11c7a8cf92ec1e2abdf26767b2ff1af3530d
steffenk <steffen@kolmer.net>
2016-11-12 02:06:39 +0100

    refactor(typings): updated type definition

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,32 +1,128 @@
 // index.d.ts
 
+type Logger = {
+    /*
+      A custom info logging function.
+    */
+    info?: (message : string) => void,
+    /*
+      A trace info logging function.
+    */
+    trace?: (message : string) => void,
+    /*
+      A warn info logging function.
+    */
+    warn?: (message : string) => void,
+    /*
+      A error info logging function.
+    */
+    error?: (message : string) => void,
+    /*
+      Log to default console.
+    */
+    toConsole?: boolean
+};
+
+type ExportOptions = {
+    /**
+     * The root directory of your source files.
+     */
+    rootPath: string ,
+    /**
+     * The full path of a source file.
+     */
+    filePath?: string,
+    /**
+     * A regex to filter source files by name or extension. Defaults to `\.jsx?$`.
+     */
+    filter?: string,
+    /**
+     * A custom logger.
+     */
+    logger?: Logger,
+    /**
+     * A progress callback.
+     */
+    progress?: (current: number, total: number, name: string) => void,
+    /**
+     * A custom preprocessor like `./preprocessors/typescript`.
+     */
+    preprocessor?: string,
+    /**
+     * A custom babylon configuration.
+     */
+    babylonConfig?: {}
+};
+
+type SchemaOptions = {
+    /**
+     * The root directory of your source files.
+     */
+    rootPath: string ,
+    /**
+     * The target path of the JSON schema.
+     */
+    schemaPath?: string,
+    /**
+     * A regex to filter source files by name or extension. Defaults to `\.jsx?$`.
+     */
+    filter?: string,
+    /**
+     * A custom logger.
+     */
+    logger?: Logger,
+    /**
+     * A progress callback.
+     */
+    progress?: (current: number, total: number, name: string) => void,
+    /**
+     * A custom preprocessor like `./preprocessors/typescript`.
+     */
+    preprocessor?: string,
+    /**
+     * A custom babylon configuration.
+     */
+    babylonConfig?: {}
+};
+
+type ValidateOptions = {
+    /**
+     * The root directory of your source files.
+     */
+    rootPath: string ,
+    /**
+     * The target path of the JSON schema.
+     */
+    schemaPath?: string,
+    /**
+     * A regex to filter source files by name or extension. Defaults to `\.jsx?$`.
+     */
+    logger?: Logger,
+    /**
+     * A progress callback.
+     */
+    progress?: (current: number, total: number, name: string) => void
+};
+
 /**
  * Generates a JSON schema based on i18n tagged template literals.
  * If no target schema is provided the json will be returned via callback output param.
  *
- * @param path Base path of the source directory.
- * @param filter File extension filter as regex string. e.g. \\.jsx?$
- * @param schema Path to the generated .json file. If no target schema is provided the json will be returned via callback output param
- * @param callback A callback function.
+ * @param options The schema options.
  */
-export default function (path: string, filter: string, schema?: string, callback?: (output: string, type: 'info' | 'warn' | 'error' | 'success' | 'debug' | 'trace') => void) 
+export function generateTranslationSchema(options: SchemaOptions) : Promise<{}>
 
 /**
  * Generates an array of all i18n tagged template literals in a file.
  * The array will be returned as json via callback output param.
  *
- * @param rootPath Root path.
- * @param path Path of the JavaScript source file or directory.
- * @param logger A logger function that accepts logging message and type 'info', 'warn', 'error', 'success', 'debug' or 'trace'.
- * @param callback A callback function.
+ * @param options The translation options.
  */
-export function exportTranslationKeys(rootPath: string, path: string, logger?: (message: string, type: 'info' | 'warn' | 'error' | 'success' | 'debug' | 'trace') => void, callback?: (output: string) => void)
+export function exportTranslationKeys(options: ExportOptions) : Promise<{}>
 
 /**
  * Validates a translation file or folder and calculates translation coverage
  *
- * @param rootPath Path of the translation file or directory.
- * @param schemaPath Path of the translation schema.
- * @param callback A callback function that accepts logging message and type 'info', 'warn', 'error', 'success', 'debug' or 'trace'.
+ * @param options The validation options.
  */
-export function validateSchema(rootPath: string, schemaPath: string, callback: (message: string, type: 'info' | 'warn' | 'error' | 'success' | 'debug' | 'trace') => void)
+export function validateSchema(options: ValidateOptions) : Promise<string>
